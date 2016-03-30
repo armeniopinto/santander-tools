da50f7233854cc49a8b2d04c575d0d9a791fc182
Armenio Pinto <armeniopinto@gmail.com>
2016-03-30 23:55:04 +0100

    Improved the comments on the TXT credit reports parser.

--- txt-to-json.py
+++ txt-to-json.py
@@ -1,11 +1,30 @@
+"""
+txt-to-json.py
+
+Parses Santander UK credit card reports and produces a JSON file with the transactions.
+
+Copyright (C) 2016 by Arménio Pinto
+Please the the file /LICENSE for the license details.
+"""
+
 import argparse
 import re
 import json
 
 
 def parse_txt(input_file):
-	transactions = []
+	"""
+	Parses a TXT credit report and returns a list of transactions with the following attributes:
 
+	date - the transaction's date.
+	card - the credit card associated with the transaction.
+	type - the textual description of the transaction type.
+	location - the place where the transaction occurred.
+	description - the textual description of the transaction.
+	amount - the transaction's amount (negative value for spendings, positive for money received).
+	"""
+
+	transactions = []
 	lines = [line.rstrip("\n") for line in open(input_file)]
 	# Transactions only show-up from the 4th line onwards.
 	for line in lines[4:-1]:
@@ -19,8 +38,9 @@ def parse_txt(input_file):
 TRANSACTION_PATTERN = re.compile(r"^(\d{4}-\d{2}-\d{2})\t(\*\*\ \d{4})\t([^\t]+)\t([\d.]+)$")
 
 def parse_transaction(line):
-	transaction = {}
+	"""Parses a transaction line."""
 
+	transaction = {}
 	match = TRANSACTION_PATTERN.match(line)
 	if match:
 		transaction["date"] = match.group(1)
@@ -35,8 +55,9 @@ def parse_transaction(line):
 DESCRIPTION_PATTERN = re.compile(r"^([^\t]+)\t([^\t]+)\t([^\t]+)$")
 
 def parse_description(description):
-	attributes = {}
+	"""Parses the description field of a transaction."""
 
+	attributes = {}
 	match = DESCRIPTION_PATTERN.match(re.sub(r"\s{2,}", r"\t", description.strip()))
 	if match:
 		attributes["type"] = match.group(1)
@@ -47,10 +68,12 @@ def parse_description(description):
 
 
 def parse_arguments():
+	""" Parses the command-line arguments. """
 	parser = argparse.ArgumentParser(
 		description = "Converts Santander TXT credit reports to JSON.")
 	parser.add_argument("input_file", help = "The input TXT credit report path.")
 	parser.add_argument("output_file", help = "The output JSON credit report path.")
+
 	return parser.parse_args()
 
 
