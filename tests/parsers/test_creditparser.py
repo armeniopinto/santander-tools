0e9dfc2348dbbcc385858954a665546b0ca55f9f
Armenio Pinto <armenio.pinto@metoffice.gov.uk>
2016-04-01 15:32:03 +0100

    Added more units tests for the credit reports parser.

--- tests/parsers/test_creditparser.py
+++ tests/parsers/test_creditparser.py
@@ -1,3 +1,12 @@
+"""
+test_creditparser.py
+
+Tests the Santander UK credit card reports parser.
+
+Copyright (C) 2016 by Arménio Pinto
+Please the the file /LICENSE for the license details.
+"""
+
 import json
 from parsers import creditparser
 
@@ -8,3 +17,11 @@ def test_parse_description():
 		for test_description in json.load(test_data_file):
 			description = creditparser.parse_description(test_description["description"])
 			assert description == test_description["expected"]
+
+
+def test_parse_transaction():
+	"""Tests creditparser.parse_transaction()."""
+	with open("tests/parsers/test_credit_transactions.json") as test_data_file:
+		for test_transaction in json.load(test_data_file):
+			transaction = creditparser.parse_transaction(test_transaction["transaction"])
+			assert transaction == test_transaction["expected"]
