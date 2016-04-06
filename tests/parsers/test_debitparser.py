39df087934874b45ffc290da43320484f3118704
Armenio Pinto <armeniopinto@gmail.com>
2016-04-06 22:43:55 +0100

    Added unit tests for the debit reports parser.

--- tests/parsers/test_debitparser.py
+++ tests/parsers/test_debitparser.py
@@ -11,3 +11,17 @@ import json
 from parsers import debitparser
 
 
+def test_parse_description():
+	"""Tests debitparser.parse_description()."""
+	with open("tests/parsers/test_debit_descriptions.json") as test_data_file:
+		for test_description in json.load(test_data_file):
+			description = debitparser.parse_description(test_description["description"])
+			assert description == test_description["expected"]
+
+
+def test_parse_transaction():
+	"""Tests debitparser.parse_transaction()."""
+	with open("tests/parsers/test_debit_transactions.json") as test_data_file:
+		for test_transaction in json.load(test_data_file):
+			transaction = debitparser.parse_transaction(test_transaction["transaction"], "1234")
+			assert transaction == test_transaction["expected"]
