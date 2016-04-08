f21d858bd078e7ff98d6e7b5c7973a25495ede07
Armenio Pinto <armeniopinto@gmail.com>
2016-04-08 22:42:58 +0100

    Added merge functionality to the reports converter.

--- parsers/convert.py
+++ parsers/convert.py
@@ -8,21 +8,45 @@ Please the the file /LICENSE for the license details.
 """
 
 import argparse
+import os
 import json
 import creditparser
 import debitparser
 
 
 def convert():
+	"""Converts the supplied files."""
+
 	args = parse_arguments()
 
-	if creditparser.is_credit_report(args.input_file):
-		output = creditparser.parse_file(args.input_file)
-	elif debitparser.is_debit_report(args.input_file):
-		output = debitparser.parse_file(args.input_file)
+	merged = []
+	for report in args.reports:
+		transactions = []
+		if creditparser.is_credit_report(report):
+			transactions = creditparser.parse_file(report)
+		elif debitparser.is_debit_report(report):
+			transactions = debitparser.parse_file(report)
+		else:
+			raise Exception(report + " isn't a known report.")
 
-	with open(args.output_file, "w") as f:
-		json.dump(output, f, indent = 4, separators = (",", ": "))
+		if args.merge:
+			merged.extend(transactions)
+			print(report + " converted.")
+		else:
+			name, ext = os.path.splitext(report)
+			output = name + ".json"
+			save_transactions(transactions, output)
+			print(report + " converted to " + output)
+
+	if args.merge:
+		save_transactions(merged, args.merge)
+		print("All transactions saved to " + args.merge)
+
+
+def save_transactions(transactions, output):
+	"""Writes a bunch of transactions to a JSON file."""
+	with open(output, "w") as f:
+		json.dump(transactions, f, indent = 4, separators = (",", ": "))
 
 
 def parse_arguments():
@@ -30,8 +54,8 @@ def parse_arguments():
 
 	parser = argparse.ArgumentParser(
 		description = "Converts Santander UK account reports to JSON.")
-	parser.add_argument("input_file", help = "The input report path.")
-	parser.add_argument("output_file", help = "The output report path.")
+	parser.add_argument("reports", nargs='+', help = "the input reports paths (supports wildcards).")
+	parser.add_argument("-merge", help = "optionally merges all transactions into a single JSON file.")
 
 	return parser.parse_args()
 
