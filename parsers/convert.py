f53e841e3a646759dfd81f6b15a32747083c449d
Armenio Pinto <armeniopinto@gmail.com>
2016-04-09 23:15:16 +0100

    Refactored the reports converter a bit more.

--- parsers/convert.py
+++ parsers/convert.py
@@ -14,31 +14,24 @@ import creditparser
 import debitparser
 
 
-def convert(reports_files, merged_file):
-	"""Converts the supplied reports, optionally merging them in a single JSON file."""
+def convert(reports_files):
+	"""
+	Converts the supplied reports.
 
-	merged = []
+	:param reports_file the list of file paths to parse.
+	:returns: a dictionary with the reports file name and the respective lists of transactions.
+	"""
+
+	transactions = {}
 	for report_file in reports_files:
-		transactions = []
 		if creditparser.is_credit_report(report_file):
-			transactions = creditparser.parse_file(report_file)
+			transactions[report_file] = creditparser.parse_file(report_file)
 		elif debitparser.is_debit_report(report_file):
-			transactions = debitparser.parse_file(report_file)
+			transactions[report_file] = debitparser.parse_file(report_file)
 		else:
 			raise Exception(report_file + " isn't a known report.")
 
-		if merged_file:
-			merged.extend(transactions)
-			print(report_file + " converted.")
-		else:
-			name, ext = os.path.splitext(report_file)
-			output_file = name + ".json"
-			save_transactions(transactions, output_file)
-			print(report_file + " converted to " + output_file)
-
-	if merged_file:
-		save_transactions(merged, merged_file)
-		print("All transactions saved to " + merged_file)
+	return transactions
 
 
 def save_transactions(transactions, output_file):
@@ -48,7 +41,7 @@ def save_transactions(transactions, output_file):
 
 
 def parse_arguments():
-	""" Parses the command-line arguments. """
+	"""Parses the command-line arguments."""
 
 	parser = argparse.ArgumentParser(
 		description = "Converts Santander UK account reports to JSON.")
@@ -60,4 +53,18 @@ def parse_arguments():
 
 if __name__ == "__main__":
 	args = parse_arguments()
-	convert(args.reports, args.merge)
+
+	merged = []
+	for report_file, transactions in convert(args.reports).items():
+		if args.merge:
+			merged.extend(transactions)
+			print(report_file + " converted.")
+		else:
+			name, ext = os.path.splitext(report_file)
+			output_file = name + ".json"
+			save_transactions(transactions, output_file)
+			print(report_file + " converted to " + output_file)
+
+	if args.merge:
+		save_transactions(merged, args.merge)
+		print("All transactions saved to " + args.merge)
